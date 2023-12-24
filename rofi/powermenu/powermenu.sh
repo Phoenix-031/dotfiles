6eaaf985451be3b4a3f7b156152b9310b48205a7
Debayan Pradhan <pradhandebayan@gmail.com>
2023-12-24 11:49:02 +0530

    added fusuma nwg& update hypr and scripts

--- rofi/powermenu/powermenu.sh
+++ rofi/powermenu/powermenu.sh
@@ -23,23 +23,23 @@ rofi_cmd() {
 		-theme ${dir}/${theme}.rasi
 }
 
-# Confirmation CMD
-confirm_cmd() {
-	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 250px;}' \
-		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
-		-theme-str 'listview {columns: 2; lines: 1;}' \
-		-theme-str 'element-text {horizontal-align: 0.5;}' \
-		-theme-str 'textbox {horizontal-align: 0.5;}' \
-		-dmenu \
-		-p 'Confirmation' \
-		-mesg 'Are you Sure?' \
-		-theme ${dir}/${theme}.rasi
-}
+# # Confirmation CMD
+# confirm_cmd() {
+# 	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 250px;}' \
+# 		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
+# 		-theme-str 'listview {columns: 2; lines: 1;}' \
+# 		-theme-str 'element-text {horizontal-align: 0.5;}' \
+# 		-theme-str 'textbox {horizontal-align: 0.5;}' \
+# 		-dmenu \
+# 		-p 'Confirmation' \
+# 		-mesg 'Are you Sure?' \
+# 		-theme ${dir}/${theme}.rasi
+# }
 
-# Ask for confirmation
-confirm_exit() {
-	echo -e "$yes\n$no" | confirm_cmd
-}
+# # Ask for confirmation
+# confirm_exit() {
+# 	echo -e "$yes\n$no" | confirm_cmd
+# }
 
 # Pass variables to rofi dmenu
 run_rofi() {
